592865e61c1d927d83b6fb7b3502579ee69336e2
Victor Yuji Muramoto <victor.muramoto@concordia.com.br>
2015-10-05 18:32:56 -0300

    Criando primeiro servico

---
+++ WCFPassagem/WCFPassagem/ClienteDAO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFPassagem
+{
+    public class ClienteDAO
+    {
+        private static List<Cliente> dados = new List<Cliente>();
+
+        public void Add(Cliente cliente)
+        {
+            ClienteDAO.dados.Add(cliente);
+        }
+
+        public Cliente Buscar(string nome)
+        {
+            return (Cliente) dados.Select(cliente => cliente.Nome.Equals(nome));
+            
+        }
+
+
+    }
+}
---
+++ WCFPassagem/WCFPassagem/ICliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFPassagem
+{
+    [ServiceContract]
+    public interface ICliente
+    {
+        
+        void Add(Cliente cliente);
+        Cliente Buscar(string nome);
+
+
+    }
+}
